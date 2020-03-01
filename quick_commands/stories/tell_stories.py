6e1825e44beda0e5ee1c67eb736f9dc5c3193ff7
NoahSediqe <noahsediqe@gmial.com>
2020-02-29 20:44:29 -0500

    stories

---
+++ quick_commands/stories/tell_stories.py
@@ -0,0 +1,24 @@
+import csv
+from random import seed
+from random import randint
+import time
+
+titles = []
+stories = []
+with open('stories.csv') as csv_file:   
+    csv_reader = csv.reader(csv_file, delimiter=',') 
+    for row in csv_reader:
+        titles.append(row[0])
+        stories.append(row[1])
+
+length = len(titles)
+randomNum = randint(0,length-1)
+title = titles[randomNum]
+story = stories[randomNum]
+
+print("Lets hear a story about " + title)
+time.sleep(1)
+print(story)
+
+   
+    
